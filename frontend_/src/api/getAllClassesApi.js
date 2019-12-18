33788f05d216e0fef00ccdfc2d4cf9d93347a851
ibra <ibrahim.deba@gmila.com>
2019-12-18 19:00:50 +0100

    add chart app

---
+++ frontend_/src/api/getAllClassesApi.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import ApiBaseUrl from "../config/config";
+
+const getAllClassesApi = async () =>{
+
+    const result = await axios({
+      method: 'get',
+      url: `${ApiBaseUrl}iris/all_classe/`,
+      headers: {
+        'Access-Control-Allow-Origin' : '*',
+      }
+    });
+    return result;
+}
+
+
+export default getAllClassesApi
